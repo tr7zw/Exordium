caecc92faaa2b02efe3b5097998c10eb2fcc4e36
tr7zw <tr7zw@live.de>
2022-07-05 19:44:35 +0200

    Don't use giant buffers... now only twice as tall as it should be...

--- Shared/src/main/java/dev/tr7zw/fastergui/NametagSettings.java
+++ Shared/src/main/java/dev/tr7zw/fastergui/NametagSettings.java
@@ -5,20 +5,16 @@ import dev.tr7zw.animatedfirstperson.config.debug.FloatSetting;
 public class NametagSettings {
 
     @FloatSetting(min = 1, max = 20, step = 0.1f)
-    public float bufferWidth = 10f;
+    public float bufferWidth = 4.5f;
     @FloatSetting(min = 1, max = 500, step = 1)
-    public float bufferHeight = 100;
+    public float bufferHeight = 24;
     @FloatSetting(min = 1, max = 500, step = 1)
-    public float renderWidth = 309;
-    @FloatSetting(min = 1, max = 500, step = 1)
-    public float renderHeight = 310;
-    @FloatSetting(min = -500, max = 500, step = 1)
-    public float scaleSize = 154;
+    public float renderHeight = 16;
+    @FloatSetting(min = 0, max = 200, step = 1)
+    public float scaleSize = 9;
     
-    @FloatSetting(min = -300, max = 0, step = 0.1f)
-    public float offsetX = -154.1f;
-    @FloatSetting(min = -300, max = 0, step = 0.1f)
-    public float offsetY = -154.9f;
+    @FloatSetting(min = -1, max = 1, step = 0.01f)
+    public float heightscale = -0.12f;
 
     
 }
