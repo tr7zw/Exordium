4f824367a6005f194033ae4b1eb042d3385a1bee
tr7zw <tr7zw@live.de>
2022-10-20 18:11:58 +0200

    DIsable the screen buffering by default

--- Shared/src/main/java/dev/tr7zw/exordium/Config.java
+++ Shared/src/main/java/dev/tr7zw/exordium/Config.java
@@ -2,12 +2,12 @@ package dev.tr7zw.exordium;
 
 public class Config {
 
-    public int configVersion = 1;
+    public int configVersion = 2;
     public boolean enabledGui = true;
     public int targetFPSIngameGui = 20;
     public boolean enabledGuiAnimationSpeedup = true;
     public int targetFPSIngameGuiAnimated = 60;
-    public boolean enabledScreens = true;
+    public boolean enabledScreens = false;
     public int targetFPSIngameScreens = 60;
     public boolean enableSignBuffering = true;
     public int targetFPSNameTags = 60;
--- Shared/src/main/java/dev/tr7zw/exordium/ConfigUpgrader.java
+++ Shared/src/main/java/dev/tr7zw/exordium/ConfigUpgrader.java
@@ -5,6 +5,12 @@ public class ConfigUpgrader {
     public static boolean upgradeConfig(Config config) {
         boolean changed = false;
 
+        if(config.configVersion <= 1) {
+            config.enabledScreens = false;
+            config.configVersion = 2;
+            changed = true;
+        }
+        
         // check for more changes here
         
         return changed;
