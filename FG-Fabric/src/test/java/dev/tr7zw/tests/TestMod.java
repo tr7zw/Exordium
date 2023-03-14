ba6c61a823db09a00e1ddb10b6c7524f933a2a29
tr7zw <tr7zw@live.de>
2023-03-14 18:39:42 +0100

    Add tests, add tooltips

---
+++ FG-Fabric/src/test/java/dev/tr7zw/tests/TestMod.java
@@ -0,0 +1,12 @@
+package dev.tr7zw.tests;
+
+import dev.tr7zw.exordium.ExordiumModBase;
+
+public class TestMod extends ExordiumModBase {
+
+    @Override
+    public void initModloader() {
+
+    }
+
+}
