b19bf7e2e9a44f249fbe16c6f7c3397c25256be9
tr7zw <tr7zw@live.de>
2023-06-29 11:25:05 +0200

    Move entrypoint #83

--- FG-Fabric/src/main/java/dev/tr7zw/exordium/ExordiumMod.java
+++ FG-Fabric/src/main/java/dev/tr7zw/exordium/ExordiumMod.java
@@ -1,7 +1,5 @@
 package dev.tr7zw.exordium;
 
-import com.llamalad7.mixinextras.MixinExtrasBootstrap;
-
 import net.fabricmc.api.ClientModInitializer;
 
 public class ExordiumMod extends ExordiumModBase implements ClientModInitializer {
@@ -9,7 +7,6 @@ public class ExordiumMod extends ExordiumModBase implements ClientModInitializer
     @Override
     public void onInitializeClient() {
         super.onInitialize();
-        MixinExtrasBootstrap.init();
     }
 
     @Override
